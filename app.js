b30ad3bfb8fa44a80579661a3dad093cfc9e57d9
Metinzeren <zeren0760@hotmail.com>
2023-02-07 20:30:40 +0300

    committt

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
 const userRoute = require("./routes/userRoute");
 const app = express();
-const session = require("express-session");
 const cookieParser = require("cookie-parser");
 dotenv.config();
 //dbye bağlanma
@@ -25,13 +24,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({}));
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  session({
-    secret: "my_keyboard_cat", // Buradaki texti değiştireceğiz.
-    resave: false,
-    saveUninitialized: true,
-  })
-);
 app.get("/", (req, res) => {
   res.status(200).send("first req");
 });
