90b9012de60f317c41e5b67ed6dfe6e12abee091
Metinzeren <zeren0760@hotmail.com>
2023-02-07 19:41:38 +0300

    dsddf

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ dotenv.config();
 //dbye bağlanma
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(process.env.DATABASE_URL || "mongodb://localhost/6326", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    process.env.DATABASE_URL || {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("bağlandı");
   });
