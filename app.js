da5e8c813d9c584e6eba874de2ccdf2a557581e0
Metinzeren <zeren0760@hotmail.com>
2023-02-07 19:56:51 +0300

    tekrar

--- app.js
+++ app.js
@@ -12,12 +12,10 @@ dotenv.config();
 //dbye bağlanma
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(
-    process.env.DATABASE_URL || {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("bağlandı");
   });
